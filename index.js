d1aa5ee813be9785b31eb7f7d24d844ced03793d
MRoyM <michael_mather@hotmail.com>
2017-03-09 16:35:04 -0330

    Start on redirect request

--- index.js
+++ index.js
@@ -5,31 +5,37 @@ var app = express();
 var mongo_url = "mongodb://localhost:27017/data" //process.env.MONGOLAB_URI;
 var mongoClient = mongo.MongoClient
 
-app.get("/url/:url", function(request, response){
+app.post("/url/:url", function(request, response){
   var url = request.params.url;
   console.log("Receiving request");
 
   mongoClient.connect(mongo_url, function(err, db){
     if (err){
       console.log(err);
-      response.statuvas(404).send("Error processing request");
+      response.status(404).send("Error processing request");
     }
     else{
 
       var collection = db.collection("urls");
+
       var exists = true;
-      var randNum;
+      var randNum = Math.round(Math.random()*10000);
+      /**
       while (exists){
-        randNum = Math.round(Math.random()*10000)
+        collection.find({extension: randNum}).toArray(function(err, doc){
+          randNum = Math.round(Math.random()*10000)
+          console.log(randNum);
+          console.log(doc.length);
 
-        collection.find({extension: randNum}).toArra(function(err, doc){
-          if (docs.length==0){
+          if (doc.length==0){
             exists = false;
+            console.log("Unique key " + randNum);
           }
-        });
+          });
       }
+      */
 
-      var shorter = {long_url: url, extension randNum};
+      var shorter = {long_url: url, extension: randNum};
 
       collection.insert(shorter, function(err, docs){
         if (err){
@@ -44,6 +50,30 @@ app.get("/url/:url", function(request, response){
     }
 
   });
+  response.send("Done.");
+
+});
+
+
+app.get("/go/:ext", function(request, response){
+
+  var ext = request.params.ext;
+  var redirectURL;
+  console.log("Working...");
+  mongoClient.connect(mongo_url, function(err, db){
+    if (err){
+      console.log(err);
+    }
+    var collection = db.collection("urls");
+
+    collection.find({extension: ext}).toArray(function(err, doc){
+
+      redirectURL = doc.long_url;
+      db.close()
+    });
+
+  });
+  response.redirect(redirectURL);
 
 });
 
