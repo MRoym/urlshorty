1ff96f476967439ed3430752de3eeb5471e257fd
MRoyM <michael_mather@hotmail.com>
2017-03-09 14:58:07 -0330

    Configure mongo database

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ var mongo = require("mongodb");
 var port = process.env.PORT || 3000;
 var app = express();
 var mongo_url = "mongodb://localhost:27017/data" //process.env.MONGOLAB_URI;
-var mongoClient = mongo.mongoClient
+var mongoClient = mongo.MongoClient
 
-app.get("/:url", function(request, response){
+app.get("/url/:url", function(request, response){
   var url = request.params.url;
+  console.log("Receiving request");
 
   mongoClient.connect(mongo_url, function(err, db){
     if (err){
       console.log(err);
-      response.status(404).send("Error processing request");
+      response.statuvas(404).send("Error processing request");
     }
     else{
 
@@ -21,32 +22,28 @@ app.get("/:url", function(request, response){
       while (exists){
         randNum = Math.round(Math.random()*10000)
 
-        collection.find({short_url: "https://safe-eyrie-53822.herokuapp.com/" + randNum}, function(err, doc){
-          if (!docs.length){
+        collection.find({extension: randNum}).toArra(function(err, doc){
+          if (docs.length==0){
             exists = false;
           }
         });
       }
 
-      var shorter = {long_url: url, short_url: "https://safe-eyrie-53822.herokuapp.com/" + randNum};
+      var shorter = {long_url: url, extension randNum};
 
-      collection.insert(shorter, fucntion(err, docs){
-
+      collection.insert(shorter, function(err, docs){
         if (err){
           console.log(err);
         }
         else {
           console.log("Inserted new short url")
         }
+        db.close();
       });
 
-
-
     }
-    db.close();
 
   });
-
 
 });
 
