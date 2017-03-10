2c5fb449c18bb745e14437ceaa2b4e3abe6f2d15
MRoyM <michael_mather@hotmail.com>
2017-03-10 18:28:30 -0330

    Add mongoose schemas

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ var mongoose = require("mongoose");
 var port = process.env.PORT || 3000;
 var app = express();
 var mongo_url = "mongodb://localhost:27017/data" //process.env.MONGOLAB_URI;
-var mongoClient = mongo.MongoClient
 
 var urlSchema = new mongoose.Schema({long_url: String,
-                                    extension: Number});
+                                    extension: String});
 
 var urlModel = mongoose.model("URL Model", urlSchema);
 
@@ -16,9 +15,45 @@ app.get("/", function(request, response){
   response.send("Home page!");
 })
 
-app.get("/url/:url", function(request, response){
+app.get("/url/*", function(request, response){
 
-  response.send(request.params.url);
+  mongoose.connect(mongo_url, function(err, result){
+    if (err){
+      console.log(err)
+    }
+    else {
+      console.log("Connected to " + mongo_url);
+    }
+  });
+
+  var url = request.params[0];
+  var query = urlModel.find({long_url: url});
+
+  query.exec(function(err, doc){
+    if (err){
+      console.log(err)
+    }
+    else {
+      var extension = function(){
+
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXY1234567890";
+        var ext = "";
+        for (var i = 0; i < 4; i ++){
+          ext += chars[(Math.random()*chars.length).toFixed(0)]
+        }
+        return ext;
+      }
+
+
+      var newUrl =  new urlModel({long_url: url,
+                                extension: extension})
+
+      newUrl.save(function (err) {if (err) console.log ('Error on save!')});
+
+    }
+
+
+  });
 });
 
 
